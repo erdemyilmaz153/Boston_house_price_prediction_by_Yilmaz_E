df615e3f1f31c742453be5cbea4afaad4032f0e6
Erdem <Erdem@DESKTOP-N2KOQQ1>
2024-09-07 19:59:40 +0300

    Test

---
+++ helperFunctions.py
@@ -0,0 +1,175 @@
+
+def check_data(dataframe,head=5):
+    print(20*"-" + "Information".center(20) + 20*"-")
+    print(dataframe.info())
+    print(20*"-" + "Data Shape".center(20) + 20*"-")
+    print(dataframe.shape)
+    print("\n" + 20*"-" + "The First 5 Data".center(20) + 20*"-")
+    print(dataframe.head())
+    print("\n" + 20 * "-" + "The Last 5 Data".center(20) + 20 * "-")
+    print(dataframe.tail())
+    print("\n" + 20 * "-" + "Missing Values".center(20) + 20 * "-")
+    print(dataframe.isnull().sum())
+    print("\n" + 40 * "-" + "Describe the Data".center(40) + 40 * "-")
+    print(dataframe.describe([0.01, 0.05, 0.10, 0.50, 0.75, 0.90, 0.95, 0.99]).T)
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.ylabel(numerical_col)
+        plt.show(block=True)
+
+def target_summary_with_num(dataframe,target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col:"mean"}), end="\n\n")
+    print("###################################")
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({'TARGET_MEAN': dataframe.groupby(categorical_col)[target].mean()}), end='\n\n\n')
+
+def correlation_matrix(df,cols):
+    fig = plt.gcf()
+    fig.set_size_inches(10, 8)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
+    fig = sns.heatmap(df[cols].corr(), annot=True, linewidths=0.5, annot_kws={'size': 12}, linecolor='w', cmap='RdBu')
+    plt.show(block=True)
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.7):
+    quartile1 = dataframe[col_name].quantile(0.25)
+    quartile3 = dataframe[col_name].quantile(0.75)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def check_outlier(dataframe, col_name):  # percentile degistirmek isteseydik buraya parametre olarak eklemeliydik.
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):  # 10 ve 20 sinirlari kisisel kararlardir.
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > cat_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # numerical
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f"cat_cols: {len(cat_cols)}")
+    print(f"num_cols: {len(num_cols)}")
+    print(f"cat_but_car: {len(cat_but_car)}")
+    print(f"num_but_car: {len(num_but_cat)}")
+
+    return cat_cols, num_cols, cat_but_car
+
+
+def grab_outliers(dataframe, col_name, index=False):
+    low, up = outlier_thresholds(dataframe, col_name)
+    if dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0] > 10:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head())
+    else:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head())
+
+    if index:
+        outlier_index = dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].index
+        return outlier_index
+
+
+def remove_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    df_without_outliers = dataframe[~((dataframe[col_name] < low_limit) | (dataframe[col_name] > up_limit))]
+    return df_without_outliers
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=True):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+
+def rare_analyser(dataframe, target, cat_cols):
+    for col in cat_cols:
+        print(col, ':', len(dataframe[col].value_counts()))
+        print(pd.DataFrame({"COUNT": dataframe[col].value_counts(),
+                            "RATIO": dataframe[col].value_counts() / len(dataframe),
+                            "TARGET_MEAN": dataframe.groupby(col)[target].mean()}), end="\n\n\n")
+
+
+def rare_encoder(dataframe, rare_perc):
+    temp_df = dataframe.copy()  # dataframemimizi kopyaladik ve temp_df'de yaptiklarimiz ana tabloyu etkilemeyecek.
+
+    rare_columns = [col for col in temp_df.columns if temp_df[col].dtypes == "O"
+                    and (temp_df[col].value_counts() / len(temp_df) < rare_perc).any(axis=None)]
+
+    for var in rare_columns:
+        tmp = temp_df[var].value_counts() / len(temp_df)
+        rare_labels = tmp[tmp < rare_perc].index
+        temp_df[var] = np.where(temp_df[var].isin(rare_labels), "Rare", temp_df[var])
+
+    return temp_df
+
+
+def high_correlated_col(dataframe, plot=False, corr_th=0.9):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_mask = np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool_)
+    upper_triangle_matrix = cor_matrix.mask(upper_triangle_mask)
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > 0.90)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show()
+    return drop_list
+
+
+def replace_with_thresholds1(dataframe, variable):
+    # Get the lower and upper limits for replacing outliers
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    low_limit = max(low_limit, 0)
+    # Additional condition to ensure variable cannot be below zero
+    # if variable not in ['variable1', 'variable2']:
+    # low_limit = max(low_limit, 0)  # Ensure low_limit is not below zero
+
+    # Replace values below the lower limit with the lower limit
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+
+    # Replace values above the upper limit with the upper limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
